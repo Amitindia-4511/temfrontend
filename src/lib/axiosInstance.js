2c45e9f3768108623a5e7489fdeefc5e9a4dce69
Amit <amitindia4511@gmail.com>
2025-03-13 10:14:58 +0530

    final again

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -3,7 +3,7 @@ import { server } from "../constants/config";
 
 console.log(server);
 const axiosInstance = axios.create({
-  baseURL: "https://tempbackend-47ip.onrender.com",
+  baseURL: "https://tempbackend-47ip.onrender.com/api/",
   withCredentials: true,
 });
 
